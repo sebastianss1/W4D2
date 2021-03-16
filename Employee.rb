72f2bed14b73b5c7f61eaa5b27e4abbd014cc3b4
akerpelm <akerpelm@u.rochester.edu>
2021-03-16 15:10:27 -0400

    Complete Class Inheritance

--- Employee.rb
+++ Employee.rb
@@ -1,18 +1,36 @@
 class Employee 
 
-    attr_reader :name, :title, :salary, :boss 
+    attr_accessor :name, :title, :salary, :boss 
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary)
         @name = name
         @title = title
         @salary = salary
-        @boss = boss 
+        @boss = nil 
     end 
 
     def bonus(multiplier)
         bonus = @salary * multiplier
     end 
 
+    def boss= (manager)
+        @boss = manager
+        boss.employees_managed << self
+    end
 
+    # def inspect
+    #     "#{name}, #{boss}"
+        
+    # end
+
+        
+
+
+
+    # #private method is_manager? 
+    #     #if !title includes ""
+    #    employees_managed <<  new_employee ??
+
+    
 
 end 
--- Manager.rb
+++ Manager.rb
@@ -1,18 +1,46 @@
 require_relative "./employee.rb"
 
 
-class Manager < Employee 
+class Manager < Employee
 
-    def initialize
-        @employees_managed = []
+    attr_accessor :name, :title, :salary, :employees_managed
+    def initialize(name, title, salary)
         super
+        @employees_managed = []
+        #if another instance has name of manager as boss, 
+        #employees_managed << employee
         #holds an array of employees assigned to the manager 
         #manager must report to higher level 
     end 
 
-    def bonus 
-         #bonus = (sum of salaries of employees_managed) * multiplier
+    def bonus(multiplier)
+        return managed_salaries * multiplier
+    end
+
+    def managed_salaries
+        total_salary = 0
+
+        self.employees_managed.each do |el|
+            if el.is_a?(Manager)
+                total_salary += el.managed_salaries + el.salary
+            else
+                total_salary += el.salary
+            end
+        end
+        total_salary
     end 
+
+            # total_salary = 0
+            # manager = [f]
+
+            # until manager.empty?
+            #     current_manager = manager.shift
+            #         current_manager.each do |person|
+            #             total_salary += person.salary
+            #             manager << person
+        # super ???? 
+        #  bonus = (sum of salaries of employees_managed) * multiplier
+         #go through levels, find salaries, * multi 
 
 
 
@@ -25,3 +53,13 @@ end
 # create Manager.new with same attributes as employees 
 #if not
 #proceed with employee attributes only
+
+
+ned = Manager.new("Ned", "Founder", 1000000)
+darren = Manager.new("Darren", "TA Manager", 78000)
+shawna = Employee.new("Shawna", 'TA', 12000)
+david = Employee.new("David", 'TA', 10000)
+
+david.boss = darren
+shawna.boss = darren
+darren.boss = ned
